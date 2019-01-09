07f75b1d6a93f21f3ebfb0165a464a4a4eb64bfe
madsmansour <42993941+madsmansour@users.noreply.github.com>
2019-01-09 12:39:53 +0100

    test

--- src/SimpleGame.java
+++ src/SimpleGame.java
@@ -1,13 +1,86 @@
 import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.entity.Entities;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.input.Input;
+import com.almasb.fxgl.input.UserAction;
 import com.almasb.fxgl.settings.GameSettings;
+import javafx.scene.input.KeyCode;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+
+import java.util.Map;
 
 public class SimpleGame extends GameApplication {
     @Override
     protected void initSettings(GameSettings gameSettings) {
-        gameSettings.setTitle("SimpleGame");
+        gameSettings.setTitle("Simple Game");
         gameSettings.setWidth(600);
         gameSettings.setHeight(600);
+        gameSettings.setVersion("0.1");
     }
+    private Entity player;
+
+    @Override
+    protected void initGame() {
+        player = Entities.builder()
+                .at(300, 300)
+                .viewFromNode(new Rectangle(25, 25, Color.DARKGREEN))
+                .buildAndAttach(getGameWorld());
+    }
+
+    @Override
+    protected void initInput() {
+        Input input = getInput();
+
+        input.addAction(new UserAction("Move Right") {
+            @Override
+            protected void onAction() {
+                player.translateX(5);
+                getGameState().increment("pixelsMoved", +5);
+            }
+        }, KeyCode.D);
+
+        input.addAction(new UserAction("Move Left") {
+            @Override
+            protected void onAction() {
+                player.translateX(-5); // move left 5 pixels
+                getGameState().increment("pixelsMoved", +5);
+            }
+        }, KeyCode.A);
+
+        input.addAction(new UserAction("Move Up") {
+            @Override
+            protected void onAction() {
+                player.translateY(-5); // move up 5 pixels
+                getGameState().increment("pixelsMoved", +5);
+            }
+        }, KeyCode.W);
+
+        input.addAction(new UserAction("Move Down") {
+            @Override
+            protected void onAction() {
+                player.translateY(5); // move down 5 pixels
+                getGameState().increment("pixelsMoved", +5);
+            }
+        }, KeyCode.S);
+    }
+    @Override
+    protected void initUI() {
+        Text textPixels = new Text();
+        textPixels.setTranslateX(50); // x = 50
+        textPixels.setTranslateY(100); // y = 100
+
+        getGameScene().addUINode(textPixels); // add to the scene graph
+        textPixels.textProperty().bind(getGameState().intProperty("pixelsMoved").asString());
+    }
+
+    @Override
+    protected void initGameVars(Map<String, Object> vars) {
+        vars.put("pixelsMoved", 0);
+    }
+
+
     public static void main(String[] args) {
         launch(args);
     }
